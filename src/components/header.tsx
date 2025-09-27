d410b291788a9d30e26590b67961ab21c71eb292
Alen Damadzic <166737547+alendamadzic@users.noreply.github.com>
2025-09-27 20:32:23 +0100

    Link back to homepage

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import { PlayerSearch } from '@/components/search/player-search';
 import { cn } from '@/lib/utils';
 
 export function Header({ className }: { className?: string }) {
   return (
     <header className={cn('flex flex-col gap-4', className)}>
-      <span className="block text-center text-primary font-bold">squad number.</span>
+      <Link href="/">
+        <span className="block text-center text-primary font-bold">squad number.</span>
+      </Link>
       <PlayerSearch className="w-full" />
     </header>
   );
